63a41cb935a9517026fb64eba59edc8793b925d5
Marciparci <marcelturtschan@t-online.de>
2022-08-23 08:49:17 +0200

    Projekt Erstellung

---
+++ src/angular-components/hello-world.component.ts
@@ -0,0 +1,16 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-hello-world',
+//   template: '<h1>Hello World from Angular as a Web Component!</h1>',
+// })
+
+// export class HelloWorldComponent implements OnInit {
+
+//   // constructor() { }
+
+//   ngOnInit(): void {
+//     // alert("Hello World!");
+//   }
+
+// }
